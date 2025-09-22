165ec4f3fe42e17a7323cb77905cf7d71e69ec83
Jeroen van de Geest <jeroen@vandegeest.eu>
2025-09-22 16:31:19 +0200

    Aboutpage

--- src/main/kotlin/scrumdapp/views/pages/AboutPage.kt
+++ src/main/kotlin/scrumdapp/views/pages/AboutPage.kt
@@ -2,7 +2,20 @@ package com.jeroenvdg.scrumdapp.views.pages
 
 import kotlinx.html.FlowContent
 import kotlinx.html.h1
+import kotlinx.html.h2
+import kotlinx.html.li
+import kotlinx.html.p
+import kotlinx.html.ul
 
 fun FlowContent.aboutPage() {
     h1 { +"Scrumdapp" }
+    p { +"De ultieme check-in manager voor jouwn project." }
+    p {
+        +"Scrumdapp, aka Scrum Daddy App is ontwikkelt uit frustratie om makkelijk check-ins bij te houden van je team. Wij willen hier een simpelere manier om dit bij te houden met een prachtige UI en security & privacy features by design. "
+    }
+    h2 {+"Ontwikkeld door"}
+    ul {
+        li { +"Jeroen van de Geest" }
+        li { +"Daan Meijneken" }
+    }
 }
