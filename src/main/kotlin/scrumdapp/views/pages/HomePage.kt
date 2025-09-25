2276289e7db42a57f4dfbd77be6f0ef28863f284
Jeroen van de Geest <jeroen@vandegeest.eu>
2025-09-25 15:47:15 +0200

    oops

--- src/main/kotlin/scrumdapp/views/pages/HomePage.kt
+++ src/main/kotlin/scrumdapp/views/pages/HomePage.kt
@@ -1,25 +1,37 @@
 package com.jeroenvdg.scrumdapp.views.pages
 
+import com.jeroenvdg.scrumdapp.db.Group
+import com.jeroenvdg.scrumdapp.views.components.icon
 import kotlinx.html.FlowContent
 import kotlinx.html.div
 import kotlinx.html.h1
 import kotlinx.html.h2
 import kotlinx.html.img
-import kotlinx.html.p
+import kotlinx.html.a
+import kotlinx.html.span
 
-fun FlowContent.homePage() {
-    h1 { +"Scrumdapp" }
+fun FlowContent.homePage(groups: List<Group>) {
+    div(classes="horizontal w-full justify-between align-center") {
+        h1 { +"Scrumdapp" }
+        div(classes="vertical justify-center h-full") {
+            a(classes="btn btn-red horizontal") {
+                span(classes="my-auto") { +"Nieuwe Groep" }
+                icon("add")
+            }
+        }
+    }
+
     div(classes="grid row-3 g-md") {
-        groupWidget("Weird Ways of Learning", "Jeroen van de Geest")
-        groupWidget("Bouwers Bende", "Thomas Middelbos")
-        groupWidget("Capgemini", "Steven Seagull")
+        for (group in groups) {
+            groupWidget(group)
+        }
     }
 }
 
-fun FlowContent.groupWidget(title: String, owner: String) {
-    div(classes="card") {
-        h2(classes="card-title") {+title}
-        p(classes="muted") {+owner}
+fun FlowContent.groupWidget(group: Group) {
+    a(classes="card btn-card") {
+        h2(classes="card-title") {+group.name}
+//        p(classes="muted") {+group.owner.name}
         img(alt="card image", src="/static/backgrounds/15.png", classes="card-img")
     }
 }
