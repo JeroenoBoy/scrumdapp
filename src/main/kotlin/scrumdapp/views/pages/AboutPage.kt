93cb944c4f706ee970146d8e05f49658805d60c3
TheLonelyKiwi <gamingkiwi22@gmail.com>
2025-09-28 00:15:13 +0200

    Updated about me

--- src/main/kotlin/scrumdapp/views/pages/AboutPage.kt
+++ src/main/kotlin/scrumdapp/views/pages/AboutPage.kt
@@ -9,9 +9,9 @@ import kotlinx.html.ul
 
 fun FlowContent.aboutPage() {
     h1 { +"Scrumdapp" }
-    p { +"De ultieme check-in manager voor jouwn project." }
+    p { +"De ultieme check-in manager voor jouw project." }
     p {
-        +"Scrumdapp, aka Scrum Daddy App is ontwikkelt uit frustratie om makkelijk check-ins bij te houden van je team. Wij willen hier een simpelere manier om dit bij te houden met een prachtige UI en security & privacy features by design. "
+        +"Scrumdapp, aka Scrum Daddy App is ontwikkeld uit frustratie om makkelijk check-ins bij te houden van je team. Wij willen hier een simpelere manier om dit bij te houden met een prachtige UI en security & privacy features by design. `(*>﹏<*)′ "
     }
     h2 {+"Ontwikkeld door"}
     ul {
