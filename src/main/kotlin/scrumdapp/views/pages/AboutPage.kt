25682127781e7106fb056b9983edfc152f94058a
Jeroen <jeroen@vandegeest.eu>
2025-09-29 22:47:23 +0200

    Update about

--- src/main/kotlin/scrumdapp/views/pages/AboutPage.kt
+++ src/main/kotlin/scrumdapp/views/pages/AboutPage.kt
@@ -1,15 +1,26 @@
 package com.jeroenvdg.scrumdapp.views.pages
 
+import com.jeroenvdg.scrumdapp.views.components.icon
 import kotlinx.html.FlowContent
+import kotlinx.html.a
 import kotlinx.html.b
+import kotlinx.html.classes
+import kotlinx.html.div
 import kotlinx.html.h1
 import kotlinx.html.h2
+import kotlinx.html.img
 import kotlinx.html.li
 import kotlinx.html.p
 import kotlinx.html.ul
 
 fun FlowContent.aboutPage() {
-    h1 { +"Scrumdapp" }
+    div(classes="horizontal g-lg align-center") {
+        h1 { +"Scrumdapp" }
+        a(href="https://github.com/JeroenoBoy/scrumdapp", classes="btn-gh") {
+            img(alt="github-logo", src="/static/icons/github-mark.svg")
+        }
+    }
+
     p { +"De ultieme check-in manager voor jouw project." }
     p {
         +"Scrumdapp, aka Scrum Daddy App is ontwikkeld uit frustratie om makkelijk check-ins bij te houden van je team. Wij willen hier een simpelere manier om dit bij te houden met een prachtige UI en security & privacy features by design. `(*>﹏<*)′ "
@@ -25,9 +36,20 @@ fun FlowContent.aboutPage() {
             Wij vonden van wel! En dus hier, het resultaat
         """.trimIndent()
     }
-    h2 {+"Ontwikkeld door"}
+
+    div(classes="spacer-lg")
+
+    h2 { +"Ontwikkeld door" }
     ul {
-        li { +"Jeroen van de Geest" }
-        li { +"Daan Meijneken" }
+        li { a(href="https://www.jeroenvdg.com", classes="a") { target="_blank"; +"Jeroen van de Geest"} }
+        li { a(href="https://daan.meijneken.nl/", classes="a") { target="_blank"; +"Daan Meijneken" } }
+    }
+
+    div(classes="spacer-lg")
+
+    h2 { +"Credits" }
+    ul {
+        li { +"Wallpapers: "; a(href="https://gruvbox-wallpapers.vercel.app", classes="a") { target="_blank"; +"gruvbox-wallpapers" } }
+        li { +"Ktor: "; a(href="https://www.jetbrains.com/", classes="a") { target="_blank"; +"Jetbrains 🥰" } }
     }
 }
