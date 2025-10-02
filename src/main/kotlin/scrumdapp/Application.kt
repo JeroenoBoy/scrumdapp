13a6a34854e0a1bfd25f33acfbf58edfc1a1f7f5
Jeroen <jeroen@vandegeest.eu>
2025-10-02 22:26:05 +0200

    add docenten rol

--- src/main/kotlin/scrumdapp/Application.kt
+++ src/main/kotlin/scrumdapp/Application.kt
@@ -47,7 +47,7 @@ suspend fun Application.module() {
 
     install(CallLogging)
     install(CachingHeaders) {
-        options { call, outgoingContent ->
+        options { _, outgoingContent ->
             when (outgoingContent.contentType?.withoutParameters()) {
                 ContentType.Text.CSS -> CachingOptions(CacheControl.MaxAge(maxAgeSeconds = 24 * 60 * 60))
                 ContentType.Image.WEBP -> CachingOptions(CacheControl.MaxAge(maxAgeSeconds = 24 * 60 * 60))
