cd9239135403a53002988a251a66567117450db1
TheLonelyKiwi <gamingkiwi22@gmail.com>
2025-10-17 15:48:39 +0200

    some cleanup

--- src/main/kotlin/scrumdapp/views/pages/groups/UserPage.kt
+++ src/main/kotlin/scrumdapp/views/pages/groups/UserPage.kt
@@ -46,58 +46,23 @@ fun FlowContent.userEditContent(application: Application, mySelf: GroupUser, gro
                     td(classes="text-ellipse name-field") { +mySelf.user.name}
                     td(classes="text-ellipse pl-lg") { +mySelf.permissions.displayName}
                 }
-
                 for (groupUser in groupUsers.filter { it.id != mySelf.id }.sortedBy { it.user.name }) {
                     val userPermission = groupUser.permissions
-                    tr {
-                        td(classes="text-ellipse name-field") { +groupUser.user.name }
-                        td(classes="pl-md") {
-                            select(classes="input select-role w-full text-ellipse") {
-                                name = "role-${groupUser.id}"
-                                option(classes="yellow") {
-                                    value = "-1";
-                                    if (mySelf.permissions.id >= -1) attributes["disabled"] = ""
-                                    if (userPermission.id == -1) attributes["selected"] = ""
-                                    +UserPermissions.ScrumDad.displayName
-                                }
-                                option(classes="blue") {
-                                    value = "0";
-                                    if (mySelf.permissions.id >= 0) attributes["disabled"] = ""
-                                    if (userPermission.id == 0) attributes["selected"] = ""
-                                    +UserPermissions.UserManagement.displayName
-                                }
-                                option(classes="purple") {
-                                    value = "1"
-                                    if (userPermission.id == 1) attributes["selected"] = ""
-                                    +UserPermissions.CheckinManagement.displayName
-                                }
-                                option(classes="orange") {
-                                    value = "68"
-                                    if (userPermission.id == 68) attributes["selected"] = ""
-                                    +UserPermissions.Coach.displayName
-                                }
-                                option(classes="aqua") {
-                                    value = "69";
-                                    if (userPermission.id == 69) attributes["selected"] = ""
-                                    +UserPermissions.User.displayName
-                for (user in users.filter { it.id != ownUser.userId }.sortedBy { it.name }) {
-                    val userId = user.id.toString()
-                    val userPermission = userGroups.find { it.userId == user.id }?.permissions ?: UserPermissions.User
-                    if (userPermission.id > ownUser.permissions.id) {
+                    if (userPermission.id > mySelf.permissions.id) {
                         tr {
-                            td(classes="text-ellipse name-field") { +user.name }
-                            td(classes="pl-md") {
+                            td(classes = "text-ellipse name-field") { +groupUser.user.name }
+                            td(classes = "pl-md") {
                                 select(classes = "input select-role w-full text-ellipse") {
-                                    name = "role-${user.id}"
+                                    name = "role-${groupUser.user.id}"
                                     option(classes = "yellow") {
                                         value = "-1";
-                                        if (ownUser.permissions.id >= -1) attributes["disabled"] = ""
+                                        if (mySelf.permissions.id >= -1) attributes["disabled"] = ""
                                         if (userPermission.id == -1) attributes["selected"] = ""
                                         +UserPermissions.ScrumDad.displayName
                                     }
                                     option(classes = "blue") {
                                         value = "0";
-                                        if (ownUser.permissions.id >= 0) attributes["disabled"] = ""
+                                        if (mySelf.permissions.id >= 0) attributes["disabled"] = ""
                                         if (userPermission.id == 0) attributes["selected"] = ""
                                         +UserPermissions.UserManagement.displayName
                                     }
@@ -117,19 +82,14 @@ fun FlowContent.userEditContent(application: Application, mySelf: GroupUser, gro
                                         +UserPermissions.User.displayName
                                     }
                                 }
+
                             }
 
-                        if (groupUser.id != mySelf.id) {
-                            td(classes="pl-md") {
-                                div(classes="horizontal space-between align-center") {
-                                    a(classes="btn btn-red", href="#delete-user-${groupUser.user.id}") {
-                                        icon(iconName="delete_forever", classes="bg-hard")
-                                        +"Verwijder gebruiker"
-                            if (user.id != ownUser.userId) {
-                                td(classes="pl-md") {
-                                    div(classes="horizontal space-between align-center") {
-                                        a(classes="btn btn-red", href="#delete-user-${userId}") {
-                                            icon(iconName="delete_forever", classes="bg-hard")
+                            if (groupUser.id != mySelf.id) {
+                                td(classes = "pl-md") {
+                                    div(classes = "horizontal space-between align-center") {
+                                        a(classes = "btn btn-red", href = "#delete-user-${groupUser.user.id}") {
+                                            icon(iconName = "delete_forever", classes = "bg-hard")
                                             +"Verwijder gebruiker"
                                         }
                                     }
@@ -138,7 +98,7 @@ fun FlowContent.userEditContent(application: Application, mySelf: GroupUser, gro
                         }
                     } else {
                         tr {
-                            td(classes="text-ellipse name-field") { +user.name}
+                            td(classes="text-ellipse name-field") { +groupUser.user.name}
                             td(classes="text-ellipse pl-lg") { +userPermission.displayName}
                         }
 
