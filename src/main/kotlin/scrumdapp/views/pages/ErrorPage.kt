6b64f35a2b7e0b71cfe2c2a86aa17f93dde79328
TheLonelyKiwi <gamingkiwi22@gmail.com>
2025-10-18 18:59:23 +0200

    Error page

--- src/main/kotlin/scrumdapp/views/pages/ErrorPage.kt
+++ src/main/kotlin/scrumdapp/views/pages/ErrorPage.kt
@@ -3,15 +3,20 @@ package com.jeroenvdg.scrumdapp.views.pages
 import com.jeroenvdg.scrumdapp.services.ExceptionContent
 import io.ktor.server.application.Application
 import kotlinx.html.FlowContent
+import kotlinx.html.a
 import kotlinx.html.div
 import kotlinx.html.h1
+import kotlinx.html.h2
 import kotlinx.html.p
 
 fun FlowContent.errorPage(exception: ExceptionContent) {
     div(classes="c-x") {
         div(classes="card mt-c") {
-            h1(classes="card-title text-center") {+(exception.title?:"Oops! Er is misgegaan")}
+            h1(classes="text-4xl text-center my-auto") {+exception.code.toString()}
+            h2(classes="card-title") {+(exception.title?:"Oops! Er is misgegaan")}
             p { +exception.message }
+            div(classes="spacer-lg")
+            a(href="/", classes="btn btn-blue horizontal g-lg align-center justify-center") {+"Ga terug"}
         }
     }
 }
