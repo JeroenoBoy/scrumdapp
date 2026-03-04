d9db0011f281aea9067d4b96a3ce2e348d5b4df8
TheLonelyKiwi <gamingkiwi22@gmail.com>
2026-03-04 10:15:50 +0100

    added ratelimit dependency

---
+++ src/main/kotlin/scrumdapp/services/RatelimitService.kt
@@ -0,0 +1,8 @@
+package com.jeroenvdg.scrumdapp.services
+
+import io.ktor.server.application.Application
+
+import io.ktor.server.application.install
+
+fun Application.configureRatelimitService() {
+}
